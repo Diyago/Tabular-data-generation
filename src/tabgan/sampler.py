f38fcb2af1d2d806d258c59001895cfc03240e63
Insaf Ashrapov <iashrapov@gmail.com>
2021-06-19 20:34:47 +0300

    hotfix in code

--- src/tabgan/sampler.py
+++ src/tabgan/sampler.py
@@ -157,9 +157,8 @@ class SamplerOriginal(Sampler):
             if self.cat_cols is None or num_col not in self.cat_cols:
                 min_val = test_df[num_col].quantile(self.bot_filter_quantile)
                 max_val = test_df[num_col].quantile(self.top_filter_quantile)
-            filtered_df = train_df.loc[
-                (train_df[num_col] >= min_val) & (train_df[num_col] <= max_val)
-            ]
+                filtered_df = train_df.loc[
+                (train_df[num_col] >= min_val) & (train_df[num_col] <= max_val)]
             if filtered_df.shape[0] < 10:
                 raise ValueError(
                     "After post-processing generated data's shape less than 10. For columns {} test "
