7dbf2f58158f9d40f9b53ba4f911129480fe9de6
Insaf Ashrapov <iashrapov@gmail.com>
2023-03-27 21:30:32 +0300

    added unit tests to utils.py

--- src/tabgan/utils.py
+++ src/tabgan/utils.py
@@ -25,6 +25,16 @@ def make_two_digit(num_as_str: str) -> pd.DataFrame:
 
 
 def get_year_mnth_dt_from_date(df: pd.DataFrame, date_col='Date') -> pd.DataFrame:
+    """
+    Extracts year, month, and day from a date column in a pandas DataFrame.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame.
+        date_col (str): Name of the date column.
+
+    Returns:
+        pd.DataFrame: DataFrame with year, month, and day columns added.
+    """
     df[date_col] = pd.to_datetime(df[date_col])
     df['year'] = df[date_col].dt.year
     df['month'] = df[date_col].dt.month
